319092dd643d4467a5cfd242f6f391b08a56eb2f
JasperDre <jasperdre@gmail.com>
2018-10-02 15:14:28 +0200

    Added intrepeter and token class

---
+++ Scripted/Assets/Scripts/Interpreter/Interpreter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Interpreter : MonoBehaviour
+{
+    [SerializeField]
+    private Text script;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void Run()
+    {
+        if (script.text.Length > 0)
+        {
+
+        }
+        else
+        {
+            // TODO: Add pop-up or in-game feedback
+            Debug.LogWarning("Script error!");
+        }
+    }
+}
---
+++ Scripted/Assets/Scripts/Interpreter/Token.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Token : MonoBehaviour
+{
+    enum TokenType
+    {
+        // Single-character tokens.                      
+        LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+        COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+        // One or two character tokens.                  
+        BANG, BANG_EQUAL,
+        EQUAL, EQUAL_EQUAL,
+        GREATER, GREATER_EQUAL,
+        LESS, LESS_EQUAL,
+
+        // Literals.                                     
+        IDENTIFIER, STRING, NUMBER,
+
+        // Keywords.                                     
+        AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+        PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+        // End-of-file
+        EOF
+    }
+
+    TokenType type;
+    string lexeme;
+    Object literal;
+    int line;
+
+    // Constructor
+    Token(TokenType type, string lexeme, Object literal, int line)
+    {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public string toString()
+    {
+        return type + " " + lexeme + " " + literal;
+    }
+}
